07c77d4924f795d7daa93091e97eeb0e3d8fa5cf
IoannisCleary <nicolemaniac@gmail.com>
2013-11-12 23:50:06 +0000

    Time , TimeSlot and Day

---
+++ src/Day.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+
+public class Day {
+
+	private ArrayList<TimeSlot> slots;
+public Day(){	
+	slots=new ArrayList<TimeSlot>();
+	for(int i=0;i<9;i++){
+		slots.add(new TimeSlot(new Time(i+9,00),new Time(i+10,00)));
+	}
+}
+
+public boolean check(Time b,Time e){
+	int diff=b.getHour()-e.getHour();
+	boolean c=false;
+	if (diff>1){
+		for(int i=0;i<=diff;i++){
+			if(slots.get(b.getHour()-9+i).isAvailable()){
+				c=true;
+			}
+		}
+		if (c){
+			for(int i=0;i<=diff;i++){
+				slots.get(b.getHour()-9+i).book();
+				}
+			}
+		}
+	else{
+		if(slots.get(b.getHour()-9).isAvailable()){
+			slots.get(b.getHour()-9).book();
+			c=true;
+		}
+	}
+	return c;
+}
+
+public ArrayList<TimeSlot> getSlots() {
+	return slots;
+}
+
+public void setSlots(ArrayList<TimeSlot> slots) {
+	this.slots = slots;
+}
+}
---
+++ src/TimeSlot.java
@@ -0,0 +1,40 @@
+
+public class TimeSlot {
+	private Time begin;
+	private Time end;
+	private boolean available;
+	public TimeSlot(Time b,Time e){
+		begin=b;
+		end=e;
+		available=true;
+	}
+	public TimeSlot(String b,String e){
+		begin=new Time(b);
+		end=new Time(e);
+		available=true;
+	}
+	public TimeSlot(int bh,int bm,int eh,int em){
+		begin=new Time(bh,bm);
+		end=new Time(eh,em);
+		available=true;
+	}
+	public boolean isAvailable(){
+		return available;
+	}
+	public void book(){
+		available=false;
+	}
+	public Time getBegin() {
+		return begin;
+	}
+	public void setBegin(Time begin) {
+		this.begin = begin;
+	}
+	public Time getEnd() {
+		return end;
+	}
+	public void setEnd(Time end) {
+		this.end = end;
+	}
+
+}
