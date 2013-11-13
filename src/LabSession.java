d03da4ca9f2ed3e27e67b7afb63b27e22cc09a3c
IoannisCleary <nicolemaniac@gmail.com>
2013-11-13 20:08:02 +0000

    Lab session holds number of tutors. Tutor functions assosciated with Lab Sessions

--- src/LabSession.java
+++ src/LabSession.java
@@ -2,18 +2,35 @@ import java.util.ArrayList;
 
 
 public class LabSession extends Session {
-	
+
+	private int	tutorNum;
 	private ArrayList<Tutor> tutors;
 
 	public LabSession(boolean r, boolean c, int t, String l, String crs,
 			int bh, int bm, int eh, int em, int[] y,int cap) {
-		super(r, c, t, l, crs, bh, bm, eh, em, y,cap);
+		super(r, c,l, crs, bh, bm, eh, em, y,cap);
+		tutorNum=t;
+		tutors=new ArrayList<Tutor>();
+		// TODO Auto-generated constructor stub
+	}
+	public LabSession(boolean r, boolean c,String l, String crs,
+			int bh, int bm, int eh, int em, int[] y,int cap) {
+		super(r, c,l, crs, bh, bm, eh, em, y,cap);
+		tutorNum=0;
 		tutors=new ArrayList<Tutor>();
 		// TODO Auto-generated constructor stub
 	}
 	public LabSession(boolean r, boolean c, int t, String l, String crs,
 			String b, String e, int[] y,int cap) {
-		super(r, c, t, l, crs, b,e, y,cap);
+		super(r, c,l, crs, b,e, y,cap);
+		tutorNum=t;
+		tutors=new ArrayList<Tutor>();
+		// TODO Auto-generated constructor stub
+	}
+	public LabSession(boolean r, boolean c,String l, String crs,
+			String b, String e, int[] y,int cap) {
+		super(r, c,l, crs, b,e, y,cap);
+		tutorNum=0;
 		tutors=new ArrayList<Tutor>();
 		// TODO Auto-generated constructor stub
 	}
@@ -21,8 +38,17 @@ public class LabSession extends Session {
 	public ArrayList<Tutor> getTutors() {
 		return tutors;
 	}
+	public void setTutorsNeeded(int n){
+		tutorNum=n;
+	}
+	public void moreTutors(int n){
+		tutorNum+=n;
+	}
 	public void addTutor(Tutor t) {
-		tutors.add(t);
+		if(tutors.size()==tutorNum)
+			System.out.println("Can't add anymore tutors to session");
+		else{
+		tutors.add(t);}
 	}
 	
 
