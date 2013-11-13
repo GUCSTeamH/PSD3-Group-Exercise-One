cba41a8aab6696a06f0ee773e6dfc2b5c5062db8
Michael McDonald <mcdonaldm1993@gmail.com>
2013-11-13 18:36:44 +0000

    Added User and Admin class

---
+++ src/Admin.java
@@ -0,0 +1,8 @@
+
+public class Admin extends User {
+	
+	public Admin(String userID, String forename, String surname){
+		super(userID, forename, surname);
+	}
+	
+}
---
+++ src/User.java
@@ -0,0 +1,37 @@
+
+public abstract class User {
+	private String userID;
+	private String forename;
+	private String surname;
+	
+	public User(String userID, String forename, String surname) {
+		this.userID = userID;
+		this.forename = forename;
+		this.surname = surname;l
+	}
+	
+	public String getuserID() {
+		return userID;
+	}
+	
+	public void setuserID(String userID) {
+		this.userID = userID;
+	}
+	
+	public String getForename() {
+		return forename;
+	}
+	
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+}
