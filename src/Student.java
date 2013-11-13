9ecc75f703e6c09cf68471b3160486105f32aae8
Michael McDonald <mcdonaldm1993@gmail.com>
2013-11-13 22:13:51 +0000

    Forgot to commit Student class(oops)

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,23 @@
 import java.util.LinkedList;
 
 
-public class Student {
-	public String studentID;
+public class Student extends User {
+	public String guID;
 	private LinkedList<StudentCourse> coursesTaken;
 	
 	
-	public Student(String id){
-		studentID=id;
+	public Student(String guID, String, userID, String forename, String surname){
+		super(userID, forename, surname);
+		this.guID = guID;
 		coursesTaken=new LinkedList<StudentCourse>();
+	}
+	
+	public void setGUID(String guID){
+		this.guID = guID;
+	}
+	
+	public String getGUID(){
+		return guID;
 	}
 	
 	public void addCourseRecord(StudentCourse c){
