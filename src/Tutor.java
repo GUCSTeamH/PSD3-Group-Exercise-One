7ecf9c2bc33b9e744668273e94033a7747200f8e
Andrew Tully <andrew_tully@hotmail.co.uk>
2013-11-13 18:21:48 +0000

    Add Tutor class extending User

---
+++ src/Tutor.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Tutor extends User {
+	
+	private ArrayList <Session> sessions;
+	private boolean student;
+
+	public Tutor(String id, String fname, String sname){
+		super(id, fname, sname);
+		sessions = new ArrayList<Session>();
+		student = false;
+	}
+	
+	public Tutor(String id, String fname, String sname, ArrayList<Session> sess){
+		super(id, fname, sname);
+		sessions = sess;
+		student = false;
+	}
+	
+	public ArrayList<Session> getSessions(){
+		return sessions;
+	}
+	
+	public boolean isStudent(){
+		return student;
+	}
+	
+	public void setStudent(boolean bool){
+		student = bool;
+	]
+	
+	// adds tutor if there is no conflict
+	public addTutorSession(Session s){
+		if (checkConflict(s)){
+			sessions.add(s);
+		}
+	}
+	
+	// Checks to see if tutor is available at time
+	public boolean checkConflict(Session s){
+		for (Session sess : sessions){
+			//if there is a clash:
+				//return false
+		}
+		if (student){
+			// for Session in student timetable
+				// if there is a clash
+					// return false
+		}
+		return true;
+	}
+
+}
