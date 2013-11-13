3852df6a1b6875b318067ca16ef40a172a9cc8e1
Michael McDonald <mcdonaldm1993@gmail.com>
2013-11-13 22:14:25 +0000

    Added courseID to course class

--- src/Course.java
+++ src/Course.java
@@ -3,11 +3,13 @@ import java.util.LinkedList;
 
 public class Course {
 	
+	public String courseID;
 	public String name;
 	private LinkedList<CourseStudent> students;
 	
-	public Course(String n){
-		name=n;
+	public Course(String n, String id){
+		name = n;
+		courseID = id;
 		students=new LinkedList<CourseStudent>();
 		
 	}
@@ -18,5 +20,13 @@ public class Course {
 	
 	public LinkedList<CourseStudent> getStudents(){
 		return students;
+	}
+	
+	public void setCourseID(String courseID){
+		this.courseID = courseID;
+	}
+	
+	public String getCourseID(){
+		return courseID;
 	}
 }
