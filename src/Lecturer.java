a63be44bd2e3a71a4e1d612aff967d0e1d057879
IoannisCleary <nicolemaniac@gmail.com>
2013-11-13 19:26:51 +0000

    Lecturer updated

--- src/Lecturer.java
+++ src/Lecturer.java
@@ -1,16 +1,31 @@
 import java.util.*;
 public class Lecturer extends User {
-		ArrayList<Course> courses;
-		ArrayList<ArrayList<Student>> studentsPerCourse;
-		public Lecturer(String name,String id){
-		super(name,id);
-		courses=new ArrayList<Course>();
-		studentsPerCourse=new ArrayList<ArrayList<Student>>();
+	
+		ArrayList<Course> courses=new ArrayList<Course>();
+		ArrayList<LinkedList<CourseStudent>> studentsPerCourse=new ArrayList<LinkedList<CourseStudent>>();
+
+		
+		public Lecturer(String id,String fname,String lname){
+		super(id,fname, lname);
+		}
+		
+		public Lecturer(String id,String fname,String lname, ArrayList<Course> c){
+			super(id,fname, lname);
+			courses=c;
+			for(int i=0;i<courses.size();i++){
+				studentsPerCourse.add(courses.get(i).getStudents());
+			}
+			}
+		
+		public void addCourse(Course c){
+			courses.add(c);
+			studentsPerCourse.add(c.getStudents());
 		}
 		public ArrayList<Course> getCourses() {
 			return courses;
 		}
 		public void defineSession(){
+			//code for defining session
 			return;
 		}
 }
