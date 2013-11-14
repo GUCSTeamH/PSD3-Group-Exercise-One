19d82477fb51cc1c71ab50298459ac12a8460228
Michael McDonald <mcdonaldm1993@gmail.com>
2013-11-14 16:40:00 +0000

    Fixed formatting

--- src/CourseDb.java
+++ src/CourseDb.java
@@ -5,29 +5,25 @@ import java.util.LinkedList;
 public class CourseDb {
 	
 	private static CourseDb courses=null;
-	
 	private HashMap<String,LinkedList<CourseStudent>> listOfCourses=new HashMap<String,LinkedList<CourseStudent>>();
-		private CourseDb(){}
+	
+	private CourseDb(){}
 		
-		public static CourseDb getStudentDb(){
-			if(courses==null)
-				courses=new CourseDb();
-			return courses;
-			
-		}
+	public static CourseDb getStudentDb(){
+		if(courses==null)
+			courses=new CourseDb();
+		return courses;	
+	}
 		
-		public void addRecord(String s,LinkedList<CourseStudent> students){
-			listOfCourses.put(s, students);
-			
-		}
-		//when you get the record you can export it in a .csv file
-		//or update it accordingly by adding/removing courses
-		public LinkedList<CourseStudent> getCourseRecord(String course){
-			return listOfCourses.get(course);
-		}
-
+	public void addRecord(String s,LinkedList<CourseStudent> students){
+		listOfCourses.put(s, students);
 		
-		
-
+	}
+	
+	//when you get the record you can export it in a .csv file
+	//or update it accordingly by adding/removing courses
+	public LinkedList<CourseStudent> getCourseRecord(String course){
+		return listOfCourses.get(course);
+	}
 
 }
