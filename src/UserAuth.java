7b09c4b628be8fd11a46b17bcf1c9b6ecf562f49
danicretu <cretu_daniela92@yahoo.com>
2013-11-14 19:14:58 +0000

    Simple User authentication interface, to help mock the method

---
+++ src/UserAuth.java
@@ -0,0 +1,13 @@
+
+public interface UserAuth {
+	
+	public boolean authenticate(String user, String pass);
+	
+	/*
+	 * methods to check if username is in certain database
+	 * 
+	 * will implement when time comes
+	 */
+	
+	
+}
