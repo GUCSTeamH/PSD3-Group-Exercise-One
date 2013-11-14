e963e925de2c84aa5d0fd299dbeb5c993c41baab
Andrew Tully <andrew_tully@hotmail.co.uk>
2013-11-14 17:44:38 +0000

    Added changeAttendance method to Attendance

--- src/Attendance.java
+++ src/Attendance.java
@@ -6,6 +6,7 @@ public class Attendance{
 	private ArrayList<Student> present;
 	private ArrayList<Student> mv;
 	
+	// Initially assumed to be absent
 	public Attendance(Session s){
 		absent = s.getListStudents();
 		present = new ArrayList<Student>();
@@ -22,6 +23,57 @@ public class Attendance{
 	
 	public ArrayList<Student> getMV(){
 		return mv;
+	}
+	
+	// Moves students from absent list if attendance changes.
+	public void changeAttendance(Student stud, String attendance){
+		// Checks in the absent list first. (Most likely scenario)
+		for (int i = 0; i < getAbsent().size(); i++){
+			if (getAbsent().get(i).getuserID().equals(stud.getuserID())){
+				if (attendance.equalsIgnoreCase("present")){
+					getPresent().add(getAbsent().get(i));
+					getAbsent().remove(i);
+				}
+				else if (attendance.equalsIgnoreCase("mv")){
+					getMV().add(getAbsent().get(i));
+					getAbsent().remove(i);
+				}
+				return;
+			}
+		}
+		
+		/*
+		 * Will only reach this far in the method if the user has:
+		 * 		(a) altered a students attendance that has previously been changed.
+		 *		(b) entered an incorrect student
+		 */
+		for (int i = 0; i < getPresent().size(); i++){
+			if (getPresent().get(i).getuserID().equals(stud.getuserID())){
+				if (attendance.equalsIgnoreCase("absent")){
+					getAbsent().add(getPresent().get(i));
+					getPresent().remove(i);
+				}
+				else if (attendance.equalsIgnoreCase("mv")){
+					getMV().add(getAbsent().get(i));
+					getPresent().remove(i);
+				}
+				return;
+			}
+		}
+		
+		for (int i = 0; i < getMV().size(); i++){
+			if (getMV().get(i).getuserID().equals(stud.getuserID())){
+				if (attendance.equalsIgnoreCase("absent")){
+					getAbsent().add(getPresent().get(i));
+					getMV().remove(i);
+				}
+				else if (attendance.equalsIgnoreCase("present")){
+					getPresent().add(getMV().get(i));
+					getMV().remove(i);
+				}
+				return;
+			}
+		}
 	}
 	
 	public String toString(){
